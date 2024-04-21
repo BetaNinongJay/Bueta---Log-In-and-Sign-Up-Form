f21c370ea9df8b2c22b6e92b20ba8547677683c3
BetaNinongJay <167324047+BetaNinongJay@users.noreply.github.com>
2024-04-21 22:44:18 +0800

    Add files via upload

---
+++ Tabular Form/Tabular Form/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Tabular_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label10 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.White;
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19.2258072F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80.77419F));
+            tableLayoutPanel1.Controls.Add(label16, 1, 7);
+            tableLayoutPanel1.Controls.Add(label15, 1, 6);
+            tableLayoutPanel1.Controls.Add(label14, 1, 5);
+            tableLayoutPanel1.Controls.Add(label13, 1, 4);
+            tableLayoutPanel1.Controls.Add(label10, 1, 1);
+            tableLayoutPanel1.Controls.Add(label12, 1, 3);
+            tableLayoutPanel1.Controls.Add(label11, 1, 2);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(label2, 0, 1);
+            tableLayoutPanel1.Controls.Add(label3, 0, 2);
+            tableLayoutPanel1.Controls.Add(label4, 0, 3);
+            tableLayoutPanel1.Controls.Add(label5, 0, 4);
+            tableLayoutPanel1.Controls.Add(label6, 0, 5);
+            tableLayoutPanel1.Controls.Add(label7, 0, 6);
+            tableLayoutPanel1.Controls.Add(label8, 0, 7);
+            tableLayoutPanel1.Controls.Add(label9, 1, 0);
+            tableLayoutPanel1.ForeColor = Color.White;
+            tableLayoutPanel1.Location = new Point(12, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 8;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 42.4242439F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 57.5757561F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 49F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 53F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 56F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 47F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanel1.Size = new Size(776, 426);
+            tableLayoutPanel1.TabIndex = 0;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(53, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 17);
+            label1.TabIndex = 0;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(40, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 17);
+            label2.TabIndex = 1;
+            label2.Text = "BackColor";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(46, 125);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 17);
+            label3.TabIndex = 2;
+            label3.Text = "Enabled";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.None;
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(57, 181);
+            label4.Name = "label4";
+            label4.Size = new Size(36, 17);
+            label4.TabIndex = 3;
+            label4.Text = "Font";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.None;
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(41, 233);
+            label5.Name = "label5";
+            label5.Size = new Size(68, 17);
+            label5.TabIndex = 4;
+            label5.Text = "ForeColor";
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.None;
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(58, 288);
+            label6.Name = "label6";
+            label6.Size = new Size(34, 17);
+            label6.TabIndex = 5;
+            label6.Text = "Icon";
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.None;
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(58, 341);
+            label7.Name = "label7";
+            label7.Size = new Size(34, 17);
+            label7.TabIndex = 6;
+            label7.Text = "Text";
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.None;
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Black;
+            label8.Location = new Point(31, 391);
+            label8.Name = "label8";
+            label8.Size = new Size(88, 17);
+            label8.TabIndex = 7;
+            label8.Text = "StartPosition";
+            // 
+            // label9
+            // 
+            label9.Anchor = AnchorStyles.None;
+            label9.AutoSize = true;
+            label9.BackColor = Color.White;
+            label9.ForeColor = Color.Black;
+            label9.Location = new Point(314, 15);
+            label9.Name = "label9";
+            label9.Size = new Size(297, 15);
+            label9.TabIndex = 8;
+            label9.Text = "\u007fIndicates the name used in code to identify the object.";
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            label11.Anchor = AnchorStyles.None;
+            label11.AutoSize = true;
+            label11.BackColor = Color.White;
+            label11.ForeColor = Color.Black;
+            label11.Location = new Point(352, 126);
+            label11.Name = "label11";
+            label11.Size = new Size(220, 15);
+            label11.TabIndex = 10;
+            label11.Text = "Indicates whether the control is enabled.";
+            label11.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            label12.Anchor = AnchorStyles.None;
+            label12.AutoSize = true;
+            label12.BackColor = Color.White;
+            label12.ForeColor = Color.Black;
+            label12.Location = new Point(346, 182);
+            label12.Name = "label12";
+            label12.Size = new Size(233, 15);
+            label12.TabIndex = 11;
+            label12.Text = "The font used to display text in the control.";
+            label12.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            label10.Anchor = AnchorStyles.None;
+            label10.AutoSize = true;
+            label10.BackColor = Color.White;
+            label10.ForeColor = Color.Black;
+            label10.Location = new Point(350, 66);
+            label10.Name = "label10";
+            label10.Size = new Size(225, 15);
+            label10.TabIndex = 12;
+            label10.Text = "The background color of the component.";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            label13.Anchor = AnchorStyles.None;
+            label13.AutoSize = true;
+            label13.BackColor = Color.White;
+            label13.ForeColor = Color.Black;
+            label13.Location = new Point(274, 234);
+            label13.Name = "label13";
+            label13.Size = new Size(377, 15);
+            label13.TabIndex = 13;
+            label13.Text = "The foreground color of this component, which is used to display text.";
+            label13.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            label14.Anchor = AnchorStyles.None;
+            label14.AutoSize = true;
+            label14.BackColor = Color.White;
+            label14.ForeColor = Color.Black;
+            label14.Location = new Point(154, 289);
+            label14.Name = "label14";
+            label14.Size = new Size(617, 15);
+            label14.TabIndex = 14;
+            label14.Text = "Indicates the icon for a form. This icon is displayed in the form’s system menu box and when the form is minimized.";
+            label14.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label15
+            // 
+            label15.Anchor = AnchorStyles.None;
+            label15.AutoSize = true;
+            label15.BackColor = Color.White;
+            label15.ForeColor = Color.Black;
+            label15.Location = new Point(364, 342);
+            label15.Name = "label15";
+            label15.Size = new Size(197, 15);
+            label15.TabIndex = 15;
+            label15.Text = "The text associated with the control.";
+            label15.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label16
+            // 
+            label16.Anchor = AnchorStyles.None;
+            label16.AutoSize = true;
+            label16.BackColor = Color.White;
+            label16.ForeColor = Color.Black;
+            label16.Location = new Point(314, 392);
+            label16.Name = "label16";
+            label16.Size = new Size(297, 15);
+            label16.TabIndex = 16;
+            label16.Text = "Determines the position of a form when it first appears.";
+            label16.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label16;
+        private Label label15;
+        private Label label14;
+        private Label label13;
+        private Label label10;
+        private Label label12;
+        private Label label11;
+    }
+}
---
+++ Tabular Form/Tabular Form/Form1.cs
@@ -0,0 +1,20 @@
+namespace Tabular_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
